3e7ae2524dc3ba8fc7aee5c94d422a939a8957d4
Junho Kim <slaykim.ai@gmail.com>
2024-05-14 18:10:23 +0900

    Update torch_main.py

--- pytorch_src/torch_main.py
+++ pytorch_src/torch_main.py
@@ -12,6 +12,7 @@ def parse_args():
     parser.add_argument('--iteration', type=int, default=200000)
     parser.add_argument('--img_size', type=int, default=256, help='The size of image')
     parser.add_argument('--batch_size', type=int, default=4, help='The size of batch size')
+    parser.add_argument('--seed', type=int, default=None)
     parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
 
     # network settings
